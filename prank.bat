05174938fb90a077e8040b84c856b63cb7596090
user cameleon <cameleonbspro@gmail.com>
2026-02-28 21:48:26 +0100

    Add files via upload

---
+++ prank.bat
@@ -0,0 +1,51 @@
+@echo off
+
+
+taskkill /f /im explorer.exe
+
+color 2
+
+:prank
+
+pause
+
+echo Le virus a été détecté sur votre PC
+
+pause
+
+echo Une tentative de récupération de compte est en cours…
+
+pause
+
+echo impossible de recuperer votre compte.
+
+
+pause
+
+echo Plusieurs logiciels malveillants sont installés sur le système.
+
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+pause
+
+echo c'est un prank. trkl rien n'a été installé ('_')
+
+
+pause
+
+:end prank
